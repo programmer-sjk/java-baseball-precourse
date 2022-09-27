282a93d5381fb0c2d392935b14715bf396c16c8f
sjk5766 <wjdrnr6662@gmail.com>
2022-09-27 18:51:43 +0900

    test(학습 테스트)
    요구사항 2를 만족하는 테스트 코드 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -17,4 +17,11 @@ public class StringTest {
     void 하나의_문자열이_분리된다() {
         assertThat("1".split(",")).containsExactly("1");
     }
+
+    @DisplayName("문자열을 substring 함수를 사용해 소괄호를 제거할 수 있다")
+    @Test
+    void 소괄호가_제거된다() {
+        String input = "(1,2)";
+        assertThat(input.substring(1, input.length() - 1)).isEqualTo("1,2");
+    }
 }
