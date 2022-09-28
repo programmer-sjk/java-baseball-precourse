e6392d40a4f93ec4da276eb31f9281fc41788f1b
sjk5766 <wjdrnr6662@gmail.com>
2022-09-28 14:28:33 +0900

    test(set 학습 테스트)
    요구사항 1을 만족하는 테스트 코드 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,32 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("집합의 크기를 구할 수 있다")
+    @Test
+    void 크기를_구할수있다() {
+        assertThat(numbers.size()).isEqualTo(4);
+    }
+}
