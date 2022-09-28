52c91e60c06cb48fa582b15c67e03a373610e655
sjk5766 <wjdrnr6662@gmail.com>
2022-09-28 18:20:31 +0900

    feat(기능 요구사항 구현)
    사용자의 입력값을 받는다.
    - BaseBallScreen class 추가
    - main 함수에서 제공받은 Console.readLine 함수 호출
    - README.md 파일 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,16 @@ package baseball;
 import baseball.model.Computer;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
+import baseball.view.BaseBallScreen;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static final int MIN = 100;
     public static final int MAX = 999;
 
     public static void main(String[] args) {
         Computer computer = new Computer(pickNumberInRange(MIN, MAX));
+        BaseBallScreen.inputNumber();
+        String input = Console.readLine();
     }
 }
